anestv/mail

Add virtual priority inbox
Signed-off-by: GretaD <gretadoci@gmail.com>

src/store/constants.js
@@ -22,3 +22,6 @@
 export const UNIFIED_ACCOUNT_ID = 0
 export const UNIFIED_INBOX_ID = btoa('inbox')
 export const UNIFIED_INBOX_UID = UNIFIED_ACCOUNT_ID + '-' + UNIFIED_INBOX_ID
+export const PRIORITY_ACCOUNT_ID = -1
+export const PRIORITY_INBOX_ID = btoa('priority')
+export const PRIORITY_INBOX_UID = PRIORITY_ACCOUNT_ID + '-' + PRIORITY_INBOX_ID

ahstro/nextcloud-mail

src/store/constants.js
@@ -22,3 +22,5 @@
 export const UNIFIED_ACCOUNT_ID = 0
 export const UNIFIED_INBOX_ID = btoa('inbox')
 export const UNIFIED_INBOX_UID = UNIFIED_ACCOUNT_ID + '-' + UNIFIED_INBOX_ID
+export const PRIORITY_INBOX_ID = btoa('priority')
+export const PRIORITY_INBOX_UID = UNIFIED_ACCOUNT_ID + '-' + PRIORITY_INBOX_ID